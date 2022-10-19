cc8c454914dab67c07b9fda178994711e0117604
W-L <weilguny.lukas@gmail.com>
2022-10-19 14:03:54 +0100

    update script for live runs

--- aeons_live.py
+++ aeons_live.py
@@ -1,19 +1,25 @@
-import Aeons
 from time import sleep
-import logging
-#%%
-# set up arguments
-parser = Aeons.setup_parser()
+import aeons.aeons_core
+
+parser = aeons.aeons_core.setup_parser()
 args = parser.parse_args()
-#%%
-const = Aeons.Constants()
-ar = Aeons.AeonsRun(args=args, const=const)
-logging.info("starting initialisation")
 
 
 #%%
+# DEBUG MODE
+# import os
+# wd = "/home/lukas/Desktop/Aeons/18_live"
+# param_file = "../params/live/zymolog_live.params"
+# os.chdir(wd)
+# from aeons.aeons_utils import read_args_fromfile
+# args = read_args_fromfile(parser=parser, file=param_file)
+#%%
+
+args.live = 1
+
+ar = aeons.aeons_core.AeonsRun(args=args)
+
 # initialise main loop - periodically check for new data
-logging.info('Initialisation completed.. waiting for sequencing data\n')
 try:
     while True:
         next_update = ar.process_batch_live()
@@ -23,7 +29,4 @@ try:
 
 
 except KeyboardInterrupt:
-    logging.info("exiting after keyboard interrupt.. ")
-
-
-
+    print("exiting after keyboard interrupt.. ")
