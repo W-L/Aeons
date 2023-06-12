7ac52611aa81f42a3f733ea378828b00ae88ab18
W-L <weilguny.lukas@gmail.com>
2023-06-12 11:33:55 +0100

    annotate and tidy-up

--- aeons/aeons_mapper.py
+++ aeons/aeons_mapper.py
@@ -1,30 +1,40 @@
-# STANDARD LIBRARY
 from io import StringIO
 from concurrent.futures import ThreadPoolExecutor as TPE
 import logging
-# NON STANDARD LIBRARY
+from typing import Dict, Tuple, List
+
 import mappy
-# CUSTOM
-from .aeons_paf import Paf
+
+from .aeons_paf import Paf, PafLine
 
 
 
 class Indexer:
-    """
-    simple indexing wrapper around mappy
-    """
 
-    def __init__(self, fasta, mmi, t=6):
+    def __init__(self, fasta: str, mmi: str, t: int = 6):
+        """
+        Initialize simple indexing wrapper around mm2
+
+        :param fasta: The path to the input FASTA file.
+        :param mmi: The path to the output .mmi index file.
+        :param t: The number of threads to use for indexing, defaults to 6.
+        """
         self.aligner = mappy.Aligner(fn_idx_in=fasta, fn_idx_out=mmi, preset="map-ont", n_threads=t)
 
 
 
 class LinearMapper:
-    """
-    mapping class for minimap2
-    """
 
-    def __init__(self, ref, mu=450, workers=8, default=True):
+    def __init__(self, ref: str, mu: int = 450, workers: int = 8, default: bool = True):
+        """
+        Initialize a LinearMapper object; wrapper for minimap2's mappy implementation
+        For the default case of mapping against some linear references
+
+        :param ref: The path to the reference FASTA file.
+        :param mu: Length of anchor bases for simulations, defaults to 450.
+        :param workers: The number of worker threads to use for mapping, defaults to 8.
+        :param default: Whether to use the default mappy.Aligner configuration, defaults to True.
+        """
         self.mu = mu
         self.workers = workers
         # self.aligner = mappy.Aligner(fn_idx_in=ref, preset="map-ont")  # , extra_flags=0x400)
@@ -35,33 +45,30 @@ class LinearMapper:
                                          k=13, w=5, min_cnt=2, min_chain_score=20)
 
 
-    def map_sequences(self, sequences):
-        # sequences is a dict
-        # output is a dict with PafLine objects
-        # before ingesting reads, check for overlaps and containment
-        # also used to check if any reads from the readpool are contained
+
+    def map_sequences(self, sequences: Dict[str, str]) -> Dict[str, PafLine]:
+        """
+        Map sequences to the reference and return the mapping results.
+
+        :param sequences: A dictionary of read_id: sequence items.
+        :return: A dictionary of read_id: PafLine objects.
+        """
         paf_raw = self.mappy_batch(sequences=sequences, truncate=False)
         paf_dict = Paf.parse_PAF(StringIO(paf_raw), min_len=int(self.mu / 2))
         return paf_dict
 
 
-    def mappy_batch(self, sequences, out=None, truncate=False, log=True):
-        """
-        Wrapper function that maps a full batch of reads. Can also be used for the in silico exp
-        where we truncate each read to mu bases
-        Parameters
-        ----------
-        sequences: dict
-            dict of read_id: sequence items loaded as instance of current batch
-        out: str
-            name for a file to output mappings
-        truncate: bool
-            for in silico experiment we truncate the input reads. not needed for use during sequencing
 
-        Returns
-        -------
-        alignments: str
-            PAF formatted mapping hits of all mapped input reads
+    def mappy_batch(self, sequences: dict[str, str], out: str = None, truncate: bool = False, log: bool = True) -> str:
+        """
+        Map a full batch of reads and return the PAF formatted mapping hits.
+        Can also perform simulated mappings of truncated (mu-length) reads.
+
+        :param sequences: A dictionary of read_id: sequence items.
+        :param out: The name of the file to output mappings, defaults to None.
+        :param truncate: Whether to truncate the input reads for in silico experiments, defaults to False.
+        :param log: Whether to log the number of mapped and unmapped queries, defaults to True.
+        :return: The PAF formatted mapping hits of all mapped input reads.
         """
         # container to hold the hits from all reads
         batch_alignments = []
@@ -100,17 +107,13 @@ class LinearMapper:
         return alignments
 
 
-    def map_query(self, query):
+
+    def map_query(self, query: Tuple[str, str]) -> List[str]:
         """
-        Fast mapper that takes a query and returns the mapped query.
-        Parameters
-        ----------
-        query: dict item
-            key:value pair of read_id and query
-        Returns
-        -------
-        list of str
-            List of Paf formatted mapping hits
+        Map a query and return the mapped query as a list of PAF formatted mapping hits.
+
+        :param query: A key-value pair of read_id and query.
+        :return: A list of PAF formatted mapping hits.
         """
         results = []
         read_id, seq = query
@@ -123,58 +126,5 @@ class LinearMapper:
         return results
 
 
-class ValidationMapping:
 
-    def __init__(self, mapper, merged_seq, seqpool):
-        # merged_seq is newly merged sequence that we want to validate - Sequence()
-        self.merged_seq = merged_seq
-        # persistent linear mapper - created in AeonsRun
-        self.mapper = mapper
-        # we need the seqpool to grab the component sequences
-        self.seqpool = seqpool
-        # grab component sequences
-        component_sequences = self.grab_components(self.merged_seq)
-        # map the component seqs
-        self.component_paf_dict = self.map_components_to_reference(component_sequences)
-        self.component_plot = self.plot_components(self.component_paf_dict, "comp")
-        # map the components onto the new merged sequence
-        self.merger_paf_dict = self.map_components_to_merged(component_sequences)
-        self.merger_plot = self.plot_components(self.merger_paf_dict, "merge")
-
-
-    def grab_components(self, merged_seq):
-        # grab the ids of components
-        merger_component_ids = merged_seq.components
-        logging.info(f'{len(merger_component_ids)} components in this merger')
-        # grab the sequences
-        component_sequences = dict()
-        for comp_id in merger_component_ids:
-            try:
-                comp_seqo = self.seqpool.sequences[comp_id]
-            except KeyError:
-                logging.info("component sequence should have been found")
-                continue
-            component_sequences[comp_id] = comp_seqo.seq
-        return component_sequences
-
-    def map_components_to_reference(self, component_sequences):
-        # map the components of the merged sequences to the reference
-        comp_paf_dict = self.mapper.map_sequences(sequences=component_sequences)
-        return comp_paf_dict
-
-    def map_components_to_merged(self, component_sequences):
-        # map components to their newly formed merged sequence
-        # make fasta from merged sequence
-        merged_fasta = f'>{self.merged_seq.header}\n{self.merged_seq.seq}'
-        with open("tmp.fa", "w") as merged_fasta_file:
-            merged_fasta_file.write(merged_fasta)
-        merge_mapper = LinearMapper(ref="tmp.fa")
-        merger_paf_dict = merge_mapper.map_sequences(sequences=component_sequences)
-        return merger_paf_dict
-
-
-    def plot_components(self, paf_dict, out):
-        # use generic paf_dict plotting method from aeons_paf
-        component_plot = Paf.plot_pafdict(paf_dict, out)
-        return component_plot
 
